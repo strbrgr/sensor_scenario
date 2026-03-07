38341cb5424996ef9f09dc02726669fd981cd833
strbrgr <jocsti@web.de>
2026-03-07 15:03:16 -0600

    Implement string to enum parsing

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,11 +1,17 @@
 use std::{
     env,
     process::{self, exit},
+    str::FromStr,
 };
 
 struct Config {
-    sensor_type: String,
+    sensor_type: SensorType,
     frequency: String,
+}
+
+enum SensorType {
+    Temperature,
+    Humidity,
 }
 
 fn main() {
@@ -27,14 +33,32 @@ impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         match args.len() {
             3 => {
-                let sensor_type = args[1].clone();
+                let sensor_type = SensorType::from_str(&args[1].clone())
+                    .map_err(|_| "Passed in <sensor_type> is not an option.")?;
+
                 let frequency = args[2].clone();
+
                 Ok(Config {
                     sensor_type,
                     frequency,
                 })
             }
             _ => Err("Usage: <sensor type> <frequency>"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseSensorTypeError;
+
+impl FromStr for SensorType {
+    type Err = ParseSensorTypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim().to_ascii_lowercase().as_str() {
+            "temperature" => Ok(SensorType::Temperature),
+            "humidity" => Ok(SensorType::Humidity),
+            _ => Err(ParseSensorTypeError),
         }
     }
 }
