a769a6c43899baec17c97abebcdc6900f2090a58
strbrgr <jocsti@web.de>
2026-03-20 18:36:46 -0500

    Add serde, serde_json and add tcp_stream to config

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,10 +1,13 @@
-use std::{env, process::exit, str::FromStr, thread::sleep, time::Duration};
+use std::{
+    env, io::Write, net::TcpStream, process::exit, str::FromStr, thread::sleep, time::Duration,
+};
 
 use generator::sensor::{SensorType, generate_sensor_reading};
 
 struct Config {
     sensor_type: SensorType,
     frequency: u8,
+    tcp_stream: TcpStream,
 }
 
 impl Config {
@@ -16,9 +19,14 @@ impl Config {
                 let frequency = args[2]
                     .parse::<u8>()
                     .map_err(|_| "<frequency> needs to be between 0-255.")?;
+
+                let tcp_stream =
+                    TcpStream::connect("127.0.0.1:8080").map_err(|_| "Error connecting via Tcp")?;
+
                 Ok(Config {
                     sensor_type,
                     frequency,
+                    tcp_stream,
                 })
             }
             _ => Err("Usage: <sensor type> <frequency>"),
@@ -26,21 +34,31 @@ impl Config {
     }
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let mut config = Config::build(&args).unwrap_or_else(|err| {
         println!("{err}");
         exit(1);
     });
 
-    run(config);
+    run(&mut config)?;
+
+    Ok(())
 }
 
-fn run(config: Config) {
+fn run(config: &mut Config) -> std::io::Result<()> {
     loop {
         let reading = generate_sensor_reading(&config.sensor_type);
-        println!("{reading}");
+        let len = reading.len() as u32;
+
+        // Send length first
+        config.tcp_stream.write_all(&len.to_be_bytes())?;
+        let reading = serde_json::to_vec(&reading)?;
+        // send actual content
+        config.tcp_stream.write_all(&reading)?;
+
+        // println!("{reading}");
         let duration = Duration::new(config.frequency as u64, 0);
         sleep(duration);
     }
