d088bec97893c8a1aee931a7a45eec3c314d8d11
strbrgr <jocsti@web.de>
2026-03-20 18:54:51 -0500

    Add serde, serde_json and handle incoming tcp streams

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{Read, Result},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    loop {
+        let mut len_buf = [0u8; 4];
+
+        if stream.read_exact(&mut len_buf).is_err() {
+            return Ok(());
+        }
+
+        let msg_len = u32::from_be_bytes(len_buf) as usize;
+
+        let mut buf = vec![0u8; msg_len];
+        stream.read_exact(&mut buf)?;
+
+        let msg: String = serde_json::from_slice(&buf)?;
+        println!("Received message:: {}", msg);
+    }
+}
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?)?;
+    }
+
+    Ok(())
 }
