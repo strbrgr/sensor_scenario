5582f862452f7ca044bd43ad4951026ee30a9222
strbrgr <jocsti@web.de>
2026-05-14 18:45:39 -0500

    Add tracing and use iggy client as Arc

--- src/gateway/main.rs
+++ src/gateway/main.rs
@@ -1,10 +1,21 @@
 use std::{
-    io::{Read, Result},
+    error::Error,
+    io::Read,
     net::{TcpListener, TcpStream},
+    sync::Arc,
     thread,
 };
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+use iggy::prelude::{
+    Client, CompressionAlgorithm, DEFAULT_ROOT_PASSWORD, DEFAULT_ROOT_USERNAME, IggyClient,
+    IggyExpiry, MaxTopicSize, StreamClient, TopicClient, UserClient,
+};
+use tracing::{info, warn};
+
+const STREAM_NAME: &str = "sample-stream";
+const TOPIC_NAME: &str = "sample-topic";
+
+fn handle_client(mut stream: TcpStream, client: Arc<IggyClient>) -> std::io::Result<()> {
     loop {
         let mut len_buf = [0u8; 4];
 
@@ -22,15 +33,48 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     }
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
     let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let client = Arc::new(IggyClient::default());
+    client.connect().await?;
+    client
+        .login_user(DEFAULT_ROOT_USERNAME, DEFAULT_ROOT_PASSWORD)
+        .await?;
+    init_system(&client).await;
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
-            let _ = handle_client(stream);
+        let client = Arc::clone(&client);
+
+        tokio::spawn(async move {
+            let _ = handle_client(stream, client);
         });
     }
 
     Ok(())
+}
+
+async fn init_system(client: &IggyClient) {
+    match client.create_stream(STREAM_NAME).await {
+        Ok(_) => info!("Stream was created."),
+        Err(_) => warn!("Stream already exists and will not be created again."),
+    }
+
+    match client
+        .create_topic(
+            &STREAM_NAME.try_into().unwrap(),
+            TOPIC_NAME,
+            1,
+            CompressionAlgorithm::default(),
+            None,
+            IggyExpiry::NeverExpire,
+            MaxTopicSize::ServerDefault,
+        )
+        .await
+    {
+        Ok(_) => info!("Topic was created."),
+        Err(_) => warn!("Topic already exists and will not be created again."),
+    }
 }
