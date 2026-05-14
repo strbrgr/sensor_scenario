c110c349ae6d5aaa41420b0e21c4c6b7760a18b9
strbrgr <jocsti@web.de>
2026-05-14 18:58:21 -0500

    Start to produce messages

--- src/gateway/main.rs
+++ src/gateway/main.rs
@@ -2,36 +2,22 @@ use std::{
     error::Error,
     io::Read,
     net::{TcpListener, TcpStream},
+    str::FromStr,
     sync::Arc,
-    thread,
+    time::Duration,
 };
 
 use iggy::prelude::{
     Client, CompressionAlgorithm, DEFAULT_ROOT_PASSWORD, DEFAULT_ROOT_USERNAME, IggyClient,
-    IggyExpiry, MaxTopicSize, StreamClient, TopicClient, UserClient,
+    IggyExpiry, IggyMessage, MaxTopicSize, MessageClient, Partitioning, StreamClient, TopicClient,
+    UserClient,
 };
+use tokio::time::sleep;
 use tracing::{info, warn};
 
 const STREAM_NAME: &str = "sample-stream";
 const TOPIC_NAME: &str = "sample-topic";
-
-fn handle_client(mut stream: TcpStream, client: Arc<IggyClient>) -> std::io::Result<()> {
-    loop {
-        let mut len_buf = [0u8; 4];
-
-        if stream.read_exact(&mut len_buf).is_err() {
-            return Ok(());
-        }
-
-        let msg_len = u32::from_be_bytes(len_buf) as usize;
-
-        let mut buf = vec![0u8; msg_len];
-        stream.read_exact(&mut buf)?;
-
-        let msg: String = serde_json::from_slice(&buf)?;
-        println!("Received message:: {}", msg);
-    }
-}
+const PARTITION_ID: u32 = 1;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -76,5 +62,68 @@ async fn init_system(client: &IggyClient) {
     {
         Ok(_) => info!("Topic was created."),
         Err(_) => warn!("Topic already exists and will not be created again."),
+    }
+}
+
+fn handle_client(mut stream: TcpStream, client: Arc<IggyClient>) -> std::io::Result<()> {
+    loop {
+        let mut len_buf = [0u8; 4];
+
+        if stream.read_exact(&mut len_buf).is_err() {
+            return Ok(());
+        }
+
+        let msg_len = u32::from_be_bytes(len_buf) as usize;
+
+        let mut buf = vec![0u8; msg_len];
+        stream.read_exact(&mut buf)?;
+
+        let msg: String = serde_json::from_slice(&buf)?;
+        let msg = IggyMessage::from(msg);
+        let mut msgs = vec![msg];
+        let partitioning = Partitioning::partition_id(PARTITION_ID);
+
+        client.send_messages(
+            &STREAM_NAME.try_into().unwrap(),
+            &TOPIC_NAME.try_into().unwrap(),
+            &partitioning,
+            &mut msgs,
+        );
+
+        // println!("Received message:: {}", msg);
+    }
+}
+
+async fn produce_messages(client: &IggyClient) -> Result<(), Box<dyn Error>> {
+    let interval = Duration::from_millis(500);
+    info!(
+        "Messages will be sent to stream: {}, topic: {}, partition: {} with interval {} ms.",
+        STREAM_NAME,
+        TOPIC_NAME,
+        PARTITION_ID,
+        interval.as_millis()
+    );
+
+    let mut current_id = 0;
+    let messages_per_batch = 10;
+    let partitioning = Partitioning::partition_id(PARTITION_ID);
+    loop {
+        let mut messages = Vec::new();
+        for _ in 0..messages_per_batch {
+            current_id += 1;
+            let payload = format!("message-{current_id}");
+            let message = IggyMessage::from_str(&payload)?;
+            messages.push(message);
+        }
+        client
+            .send_messages(
+                &STREAM_NAME.try_into().unwrap(),
+                &TOPIC_NAME.try_into().unwrap(),
+                &partitioning,
+                &mut messages,
+            )
+            .await?;
+        info!("Sent {messages_per_batch} message(s).");
+        sleep(interval).await;
     }
 }
