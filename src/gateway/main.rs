e62df0747dd18bcdaa87dffea91c5c2735ea5cb9
strbrgr <jocsti@web.de>
2026-05-16 12:36:13 -0500

    Use Tokios Tcp stream

--- src/gateway/main.rs
+++ src/gateway/main.rs
@@ -1,30 +1,24 @@
-use std::{
-    error::Error,
-    io::Read,
-    net::{TcpListener, TcpStream},
-    str::FromStr,
-    sync::Arc,
-    time::Duration,
-};
+use std::{error::Error, str::FromStr, sync::Arc};
 
 use iggy::prelude::{
     Client, CompressionAlgorithm, DEFAULT_ROOT_PASSWORD, DEFAULT_ROOT_USERNAME, Identifier,
     IggyClient, IggyDuration, IggyExpiry, IggyMessage, MaxTopicSize, MessageClient, Partitioning,
     StreamClient, TopicClient, UserClient,
 };
-use tokio::time::sleep;
+use tokio::{
+    io::AsyncReadExt,
+    net::{TcpListener, TcpStream},
+};
 use tracing::{info, warn};
 
 const STREAM_NAME: &str = "sample-stream";
 const TOPIC_NAME: &str = "sample-topic";
 const PARTITION_ID: u32 = 1;
-const BATCHES_LIMIT: u32 = 5;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt::init();
     // Incoming Tcp messages
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
 
     // Producer client
     let client = Arc::new(IggyClient::default());
@@ -33,17 +27,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .login_user(DEFAULT_ROOT_USERNAME, DEFAULT_ROOT_PASSWORD)
         .await?;
     let (stream_id, topic_id) = init_system(&client).await;
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    loop {
+        let (stream, _) = listener.accept().await?;
+
         let client = Arc::clone(&client);
 
         tokio::spawn(async move {
             let _ = handle_client(stream, client, stream_id, topic_id).await;
         });
     }
-
-    Ok(())
 }
 
 async fn init_system(client: &IggyClient) -> (u32, u32) {
@@ -101,7 +95,6 @@ async fn handle_client(
     topic_id: u32,
 ) -> Result<(), Box<dyn Error>> {
     let duration = IggyDuration::from_str("500ms")?;
-    let mut interval = tokio::time::interval(duration.get_duration());
     info!(
         "Messages will be sent to stream: {} ({}), topic: {} ({}), partition: {} with interval {}.",
         STREAM_NAME,
@@ -113,27 +106,24 @@ async fn handle_client(
     );
 
     let messages_per_batch = 10;
-    let mut sent_batches = 0;
     let partitioning = Partitioning::partition_id(PARTITION_ID);
     let mut messages = Vec::new();
 
     loop {
-        if sent_batches == BATCHES_LIMIT {
-            info!("Sent {sent_batches} batches of messages, exiting.");
-            return Ok(());
-        }
-        interval.tick().await;
-
         let mut incoming_message_len_buf = [0u8; 4];
 
-        if stream.read_exact(&mut incoming_message_len_buf).is_err() {
+        if stream
+            .read_exact(&mut incoming_message_len_buf)
+            .await
+            .is_err()
+        {
             return Ok(());
         }
 
         let incoming_message_len = u32::from_be_bytes(incoming_message_len_buf) as usize;
 
         let mut buf = vec![0u8; incoming_message_len];
-        stream.read_exact(&mut buf)?;
+        stream.read_exact(&mut buf).await?;
 
         let message: String = serde_json::from_slice(&buf)?;
         let message = IggyMessage::from(message);
@@ -149,9 +139,7 @@ async fn handle_client(
                 )
                 .await?;
 
-            sent_batches += 1;
             info!("Sent {messages_per_batch} message(s).");
-
             messages.clear();
         }
     }
